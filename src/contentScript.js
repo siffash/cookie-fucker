3338642b5a8312ab10c5d6ee5ee9ea68cb1da17c
siffash <siffash@gmail.com>
2018-07-06 22:23:02 +0300

    added new features & fixed bugs

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,16 +1,15 @@
 var
 	stop = false,
 	position = ['fixed', 'sticky'],
-	id_class = ['cookie', 'gdpr'],
+	id_class = ['cookie', 'gdpr', 'notice'],
 	specific = ['div#cnsh', 'div#taw',						// google
 				'div#ticker',								// youtube
 				'div[data-testid="cookie-policy-banner"]',	// facebook
 				'div#global-alert-queue',					// linkedin
-				'div#j-aliexpress-notice',					// aliexpress
-				'div#cmp-container-id'						// express.co.uk
+				'div#j-aliexpress-notice'					// aliexpress
 				],
-	txtinside = ['cookie', 'agree', 'accept',				// en
-				'бисквитки', 'приемам', 'съгласен',			// bg
+	txtinside = ['cookie', 'privacy settings',  			// en
+				'бисквитки', 'приемам', 'съгласен',	    	// bg
 				'kolačiće',									// bs/hr/sr
 				'piškotke',									// sl
 				'küpsiseid',								// et
@@ -26,36 +25,30 @@ main(1);
 document.onreadystatechange = () => {
 	if (document.readyState === 'complete' && !stop) {
 		main(2);
-		// if (!stop) {
-        //     const observer = new MutationObserver( records => {
-        //         records.forEach( record => {
-        //             record.addedNodes.forEach( addedNode => {console.log('CHECKING NEW NODE');
-        //                 addedNode.nodeName !== '#text' && ( chkEl(addedNode) || addedNode.querySelectorAll('*').forEach(chkEl) );
-        //             });
-        //         });
-        //     });
-        //     observer.observe(document.body, {childList: true, subtree: true});
-        //     setTimeout(() => { observer.disconnect(); }, 5000);
-		// }
+		setTimeout(main, 500, 3);
 	}
 };
 
-function removeEl(el) {console.log('>>> COOKIE FUCKER >>> REMOVING THE ELEMENT:');console.log(el);
+function removeEl(el) {
+	console.log('>>> COOKIE FUCKER >>> REMOVED THE ELEMENT:');
+	console.log(el);
+	el.parentNode.removeChild(el);
 	document.body.style.overflow = 'auto';
-	return el.parentNode.removeChild(el);
+	return true;
 }
 
-function main(inp) {console.log('>>> COOKIE FUCKER >>> RUN MAIN CHECK # ' + inp);
+function main (inp) {
+	console.log('>>> COOKIE FUCKER >>> RUN MAIN CHECK # ' + inp);
 	if (!stop) {
 		~window.location.href.indexOf('oath.com/collectConsent') && document.body.querySelector('form[action="/consent"]').submit();
 		specific.some( el => document.body.querySelector(el) && removeEl(document.body.querySelector(el)) ) && (stop = true);
 		if (!stop)
 			(function iterateNodes(current) {
 				if (current) {
-					const children = current.childNodes;
+					const children = current.children;
 					for (let i = 0, len = children.length; i < len; i++) {
 						let child = children[i];
-						!chkEl(child) && iterateNodes(child);
+						chkEl(child) ? i-- : iterateNodes(child);
 					}
 				}
 			})(document.body);
@@ -63,20 +56,41 @@ function main(inp) {console.log('>>> COOKIE FUCKER >>> RUN MAIN CHECK # ' + inp)
 }
 
 function chkEl (el) {
-	// return true if the element has been found & removed, otherwise return false
-	return (
-		// check the text inside
-		el && txtinside.some( chk => find(el.innerText, chk) ) && (
-            // check the id and the class
-			id_class.some( chk => el.id && find(el.id, chk) || el.className && find(el.className, chk) ) && removeEl(el)
-			// check the position
-			|| ~position.indexOf(window.getComputedStyle(el)['position'])
-				// click the warning's button if it's instagram
-				&& (~window.location.href.indexOf('instagram.com') ? !el.querySelector('button').click() : removeEl(el))
-		)
-	);
+	if (el instanceof Element) {
+		var el_css = window.getComputedStyle(el);
+		var rgba = /(?:[\d.]+, ){3}([\d.]+)/; // take opacity
+		// return true if the element has been found & removed, otherwise return false
+		return (
+			// check the text inside
+			txtinside.some( chk => find(el.innerText, chk) ) && (
+				// check the id and the class
+				id_class.some( chk => el.id && find(el.id, chk) || el.className && find(el.className, chk) )
+				// check the position
+				|| ~position.indexOf(el_css['position'])
+			) // click the warning's button if it's instagram
+				&& (~window.location.href.indexOf('instagram.com') ? el.querySelector('button').click() : true) // click returns false
+			|| (
+				// check whether it overlaps the whole page
+				~position.indexOf(el_css['position'])
+					&& parseFloat(el_css['width']) >= document.body.clientWidth
+					&& parseFloat(el_css['height']) >= window.innerHeight
+					&& el_css['z-index'] > 100
+					&& el_css['top'] === '0px'
+					&& el_css['left'] === '0px'
+					&& el_css['display'] !== 'none'
+					&& el_css['visibility'] !== 'hidden'
+					&& (el_css['opacity'] < 1
+						|| el_css['background-color'].match(rgba)
+						&& el_css['background-color'].match(rgba)[1] < 1
+						)
+				// for trustarc.com & websites which use its code
+				|| el.className && find(el.className, 'truste_')
+			)
+		) && removeEl(el);
+	}
+	else return false;
 }
 
 function find (inp, chk) {
-	return inp && ~inp.toLowerCase().indexOf(chk)
+	return inp && typeof inp === 'string' && ~inp.toLowerCase().indexOf(chk);
 }
