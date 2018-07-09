c9e9164c4c6a1feaa939d67c85fc39c342971f76
siffash <siffash@gmail.com>
2018-07-09 17:53:15 +0300

    Removed console.log for launching the main function

--- src/contentScript.js
+++ src/contentScript.js
@@ -22,12 +22,12 @@ var
 				'kabul'									    	                // tr
 				];
 
-main(1);
+main();
 
 document.onreadystatechange = () => {
 	if (document.readyState === 'complete') {
-		main(2);
-		setTimeout(main, 500, 3);
+		main();
+		setTimeout(main, 500);
 	}
 };
 
@@ -39,52 +39,49 @@ function removeEl(el) {
 	return true;
 }
 
-function main (inp) {
-	if (!stop) {
-		console.log('>>> COOKIE FUCKER >>> RUN MAIN CHECKING # ' + inp);
-		oath && document.body.querySelector('form[action="/consent"]').submit();
-		specific.some( el => {
-			const el_qs = document.body.querySelector(el);
-			return el_qs && removeEl(el_qs);
-		}) && (stop = true);
-		!stop && (function iterateNodes(current) {
-			if (current) {
-				const children = current.children;
-				for (let i = 0, len = children.length; i < len; i++) {
-					var el = children[i];
-					if (el instanceof Element) {
-						var el_css = window.getComputedStyle(el);
-						// check the element's position
-						( ~position.indexOf(el_css['position'])
-							// check the element's outerHTML
-							&& ( keywords.some(chk => find(el.outerHTML, chk))
-								// click the warning's button if it's instagram, otherwise true -> will remove it
-								// (click returns false)
-								&& ( instagram ? (stop = true) && el.querySelector('button').click() : true )
-								// check whether the element overlaps the whole page
-								|| ( el_css['z-index'] > 100
-									&& el_css['top'] === '0px'
-									&& el_css['left'] === '0px'
-									&& el_css['bottom'] === '0px'
-									&& el_css['right'] === '0px'
-									&& el_css['display'] !== 'none'
-									&& el_css['visibility'] !== 'hidden'
-									&& ( el_css['opacity'] < 1
-										|| el_css['background-color'].match(alpha)
-											&& el_css['background-color'].match(alpha)[1] < 1
-										|| el_css['background-color'] === 'rgb(0, 0, 0)'
-									)
+function main () {
+	!stop && oath && document.body.querySelector('form[action="/consent"]').submit();
+	!stop && specific.some( el => {
+		const el_qs = document.body.querySelector(el);
+		return el_qs && removeEl(el_qs);
+	}) && (stop = true);
+	!stop && (function iterateNodes(current) {
+		if (current) {
+			const children = current.children;
+			for (let i = 0, len = children.length; i < len; i++) {
+				var el = children[i];
+				if (el instanceof Element) {
+					var el_css = window.getComputedStyle(el);
+					// check the element's position
+					( ~position.indexOf(el_css['position'])
+						// check the element's outerHTML
+						&& ( keywords.some(chk => find(el.outerHTML, chk))
+							// click the warning's button if it's instagram, otherwise true -> will remove it
+							// (click returns false)
+							&& ( instagram ? (stop = true) && el.querySelector('button').click() : true )
+							// check whether the element overlaps the whole page
+							|| ( el_css['z-index'] > 100
+								&& el_css['top'] === '0px'
+								&& el_css['left'] === '0px'
+								&& el_css['bottom'] === '0px'
+								&& el_css['right'] === '0px'
+								&& el_css['display'] !== 'none'
+								&& el_css['visibility'] !== 'hidden'
+								&& ( el_css['opacity'] < 1
+									|| el_css['background-color'].match(alpha)
+										&& el_css['background-color'].match(alpha)[1] < 1
+									|| el_css['background-color'] === 'rgb(0, 0, 0)'
 								)
 							)
-							// for trustarc.com & websites which use its code
-							|| find(el.className, 'truste_')
-						// if the element was removed / wasn't removed
-						) && removeEl(el) ? i-- && len-- : iterateNodes(el);
-					}
+						)
+						// for trustarc.com & websites which use its code
+						|| find(el.className, 'truste_')
+					// if the element was removed / wasn't removed
+					) && removeEl(el) ? i-- && len-- : iterateNodes(el);
 				}
 			}
-		})(document.body);
-	}
+		}
+	})(document.body);
 }
 
 function find (inp, chk) {
